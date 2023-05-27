1142a5dd0e8146460a061c499af8d9a9a1889b9b
NK2023 <drugkirill.99@mail.ru>
2023-05-27 15:48:46 +0500

    Added summing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main()
 {
     int a,b;
-    cout<<"Enter two integer numbers" << endl;
-    cin >> a >> b;
+    cout <<"Enter two integer numbers" << endl;
+    cin>>a>>b;
+    int c;
+    c=a+b;
+    cout << "a+b= " << c << endl;
 }
